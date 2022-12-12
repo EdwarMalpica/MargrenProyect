98edd8325dfccc4df8176d55b27c2146164f97d3
Edwar Malpica <edwar.malpica@uptc.edu.co>
2022-12-11 23:28:53 -0500

    LoginService

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService, ContentResponseMessage } from '../services/login.service';
+import jwt_decode from "jwt-decode";
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  email: "";
+  password:"";
+  token!:ContentResponseToken;
+  state = false;
+  constructor(private loginService:LoginService, public router:Router) {
+
+  }
+
+  
 
   ngOnInit(): void {
   }
+  getDecodedAccessToken(token: string): ContentResponseToken {
+    try {
+      return jwt_decode(token);
+    } catch(Error) {
+      return new ContentResponseToken();
+    }
+  }
+    login():void {
+
+      this.loginService.login(this.email, this.password).subscribe((x:ContentResponseMessage)=>{
+       
+       console.log(x.data);
+       
+       this.state = x.state == 'SUCCESS';
+       if(this.state){
+        this.token = this.getDecodedAccessToken(x.data);
+        localStorage.setItem('role',this.token.role);
+        localStorage.setItem('email',this.token.email);
+        localStorage.setItem('id_person',this.token.id_person);
+        localStorage.setItem('exp',this.token.exp+"");
+        this.router.navigate(['/dashboard'])
+       }
+       
+        
+    }); 
+    
+    
+  }
+}
+
+export class ContentResponseToken{
+
+
+    role="";
+    email="";
+    id_person="";
+    exp:number;
 
 }
