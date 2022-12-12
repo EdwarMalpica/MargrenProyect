3b6f82a09f52775fe3d2681dea17193a9e1fba0b
Edwar Malpica <edwar.malpica@uptc.edu.co>
2022-12-12 03:01:31 -0500

    Creacion Vista mapas

---
+++ src/app/market-map-seller/market-map-seller.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-market-map-seller',
+  templateUrl: './market-map-seller.component.html',
+  styleUrls: ['./market-map-seller.component.css']
+})
+export class MarketMapSellerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
